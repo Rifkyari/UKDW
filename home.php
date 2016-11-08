54a57ec2820fbc9214923c91a3a8b9b83bebe8f6
Rifkyari <4alwaysalone@gmail.com>
2016-11-08 18:51:46 +0700

    First

---
+++ home.php
@@ -0,0 +1,57 @@
+<html>
+	<head>
+		<title>Pariwisata Yogyakarta.</title>
+		<link rel="stylesheet" href="css/grid.css" />
+		<link rel="stylesheet" href="css/style.css" />
+	</head>
+	<body>
+		<header id="header">
+			<div class="g12plus">
+				<div class="g1 col"></div>
+				<div class="g10 col">
+					<div class="g6 col">
+						<img class="logo" src="img/icon/logo.png" width="10%"/>
+						<h2 class="logo-lead">Dinas Pariwisata</h2>
+						<h1 class="logo-name">Yogyakarta.</h1>
+					</div>
+					<div class="g6 col">
+						<ul>
+							<li><a>Home</a></li>
+							<li><a>Berita</a></li>
+							<li><a>Event</a></li>
+							<li><a>Profil</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="g1 col"></div>
+			</div>
+		</header>
+		<div class="g12plus nomarg">
+			<figure>
+				<div id="slide1"></div>
+				<div id="slide2"></div>
+				<div id="slide3"></div>
+				<div id="slide4"></div>
+				<div id="slide1"></div>
+			</figure>
+		</div>
+		<div class="g12plus flash">
+			<div class="g1 col"></div>
+			<div class="g10 col">
+				<div class="g4 col center">
+					<h1 class="jogja">Jogja</h1>
+					<p class="flight">Kini anda bisa berkunjung ke Jogja kapan pun menggunakan berbagai macam maskapai penerbangan dengan sekali klik. </p>
+					<a class="btn">Flight and Airlines</a>
+				</div>
+				<div class="g8 col"></div>
+			</div>
+			<div class="g1 col"></div>
+		</div>
+		<footer></footer>
+		
+		
+		<script>
+
+		</script>
+	</body>
+</html>
