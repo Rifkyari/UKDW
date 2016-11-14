28bb725a2b07fea2a581418a341c3a0531e3fdaf
Rifkyari <4alwaysalone@gmail.com>
2016-11-14 15:24:26 +0700

    pjUKDW-5
    Tambah Modal

--- home.php
+++ home.php
@@ -50,9 +50,9 @@
 				<div class="g12plus">
 					<div class="g1 col"></div>
 					<div class="g4 col center">
-						<h1 class="jogja ">jogja</h1>
+						<h1 class="jogja">jogja</h1>
 						<p class="flight">Kini anda bisa berkunjung ke Jogja kapan pun menggunakan berbagai macam maskapai penerbangan dengan sekali klik. </p>
-						<a class="btn green">Reservasi Sekarang</a>
+						<a class="btn green" onclick="showreserve()">Reservasi Sekarang</a>
 					</div>
 					<div class="g6 col"></div>
 					<div class="g1 col"></div>
@@ -101,7 +101,9 @@
 		</div>
 		<div class="g12plus nomarg">
 			<div class="content">
+				<div class="g12 nomarg">
 				<h2 class="center">Agenda</h2>
+				<div class="g12plus">
 				<div class="g1 col"></div>
 				<div class="g10 col">
 					<div class="event-con">
@@ -202,7 +204,10 @@
 						<div class="g2 col event-more green right center">More</div>
 					</div>
 				</div>
-				<div class="g1 col"></div>
+				<div class="g1 col"></div>					
+				</div>
+				
+				</div>
 			</div>
 		</div>
 		<div class="g12plus nomarg">
@@ -215,6 +220,7 @@
 		<div class="g12plus nomarg">
 			<div class="content">
 				<h2 class="center">Berita</h2>
+				<div class="g12plus">
 				<div class="g1 col"></div>
 				<div class="g10 col">
 					<div class="event-con">
@@ -277,8 +283,165 @@
 						<div class="g2 col event-more green right center">More</div>
 					</div>
 				</div>
-				<div class="g1 col"></div>
+				<div class="g1 col"></div>					
+				</div>
 			</div>
 		</div>
+		<div class="g12plus nomarg">
+			<footer>
+				<div class="g1 col"></div>
+				<div class="g10 col ">
+					<div class="g3 col center left">
+						<img class="logo" src="img/icon/logo-bot.png" height="80%"/>
+					</div>
+					<div class="g9 col nomarg right">
+						<div class="g6 left">
+							<div class="list"></div>
+							<div class="list">
+								<div class="icon">
+									<img src="img/icon/email.png" height="100%"/>
+								</div>
+								<div class="desc">
+									<h4>pariwisata@yogyakarta.go.id</h4>
+								</div>
+							</div>
+							<div class="list">
+								<div class="icon">
+									<img src="img/icon/map.png" height="100%"/>
+								</div>
+								<div class="desc">
+									<h4>Jalan STM Pembangunan ,Mrican ,Depok ,Sleman</h4>
+								</div>
+							</div>
+							<div class="list">
+								<div class="icon">
+									<img src="img/icon/telp.png" height="100%"/>
+								</div>
+								<div class="desc">
+									<h4>+62 89 322 223 982</h4>
+								</div>
+							</div>
+							<div class="list"></div>
+						</div>
+						<div class="g6 right">
+							<form>
+								<h3>Contact Us</h3>
+								<span>Email</span><br />
+								<input type="email" ></input><br />
+								<span>Pesan</span><br />
+								<textarea draggable="false"></textarea><br />
+								<input class="green" type="submit" value="Kirim" />
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="g1 col"></div>
+			</footer>
+		</div>
+		
+		<!--modal-->
+		
+		<div class="modal" id="modalreservasi">
+			<div class="modal-content modal-animate">
+				<div class="modal-top">
+					<div class="modal-close" onclick="closereserve()">&times;</div>
+				</div>
+				<div class="g12plus">
+					<div class="g12plus">
+						<h3>Reservasi Tiket</h3>
+						<p>Dengan penerbangan harian maskapai ternama dari berbagai kota di Indonesia, pergi ke Jogja kini menjadi lebih mudah dan lebih nyaman.</p>
+						<div class="border-bot"></div>
+					</div>
+					<div class="g12plus nomarg">
+						<div class="g6 col">
+							<form>
+							<div class="g12plus">
+								<input name="flight" type="radio" checked="" value="Sekali Jalan"/><span>Sekali Jalan</span>
+								<input name="flight" type="radio" value="Pulang Pergi"/><span>Pulang Pergi</span>
+							</div>
+							<div class="g12plus">
+								<div class="g5 col">
+									<span>Dari</span>
+									<select>
+										<option>Bandung ,(BDO)</option>
+										<option>Batam ,(BTH)</option>
+										<option>Jakarta ,Cengkareng(CGK)</option>
+										<option>Jakarta ,Halim (HLP)</option>
+									</select>
+								</div>
+								<div class="g2 col center">
+									<img class="away " src="img/icon/away.png" width="50%"/>
+								</div>
+								<div class="g5 col">
+									<span>Ke</span>
+									<select>
+										<option>Yogyakarta ,(JOG)</option>
+									</select>
+								</div>
+							</div>
+							<div class="g12plus">
+								<div class="g5 col">
+									<span>Pergi</span>
+									<input type="date"/>
+								</div>
+								<div class="g2 col"></div>
+								<div class="g5 col">
+									<span>Pulang</span>
+									<input type="date"/>
+								</div>
+							</div>
+							<div class="g12plus">
+								<div class="g2 col">
+									<span>Dewasa</span>
+									<select>
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+									</select>
+								</div>
+								<div class="g1 col"></div>
+								<div class="g2 col">
+									<span>Anak</span>
+									<select>
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+									</select>
+									<a>2-12th</a>
+								</div>
+								<div class="g2 col"></div>
+								<div class="g5 col">
+									<span>Cari</span>
+									<input class="green" type="submit" value="Cari"/>
+								</div>
+							</div>
+							</form>
+						</div>
+						<div class="g6 col center fly">
+							<h1>)</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		<!--script-->
+		<script>
+		
+		/*modal*/
+		
+		function showreserve() {
+		document.getElementById("modalreservasi").style.display = "block";
+		}
+	
+		function closereserve() {
+		document.getElementById("modalreservasi").style.display = "none";
+		}
+		</script>
 	</body>
 </html>
